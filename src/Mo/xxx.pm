719fce3d4a0465d5ca1d7c6f384a858be118a52f
Ingy döt Net <ingy@cpan.org>
2011-10-03 13:32:33 +0000

    Import of INGY/Mo-xxx-0.11 from CPAN.
    gitpan-cpan-distribution: Mo-xxx
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         INGY/Mo-xxx-0.11.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

--- src/Mo/xxx.pm
+++ src/Mo/xxx.pm
@@ -1,17 +1,17 @@
 package Mo::xxx;
 my $MoPKG = "Mo::";
-$VERSION = '0.10';
+$VERSION = '0.11';
 
 use constant XXX_skip => 1;
 ${$MoPKG.'::DumpModule'} = 'YAML::XS';
 *{$MoPKG.'xxx::e'} = sub {
     my ($caller_pkg, $exports) = @_;
     $exports->{WWW} = sub { require XXX; local $XXX::DumpModule =
-        ${$MoPKG.'::DumpModule'}; XXX::WWW(@_) };
+        ${$MoPKG.DumpModule}; XXX::WWW(@_) };
     $exports->{XXX} = sub { require XXX; local $XXX::DumpModule =
-        ${$MoPKG.'::DumpModule'}; XXX::XXX(@_) };
+        ${$MoPKG.DumpModule}; XXX::XXX(@_) };
     $exports->{YYY} = sub { require XXX; local $XXX::DumpModule =
-        ${$MoPKG.'::DumpModule'}; XXX::YYY(@_) };
+        ${$MoPKG.DumpModule}; XXX::YYY(@_) };
     $exports->{ZZZ} = sub { require XXX; local $XXX::DumpModule =
-        ${$MoPKG.'::DumpModule'}; XXX::ZZZ(@_) };
+        ${$MoPKG.DumpModule}; XXX::ZZZ(@_) };
 };
